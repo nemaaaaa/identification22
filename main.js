f7543fb88a4402abb0d905c76a2b98e0afb7617a
nemaaaaa <114819031+nemaaaaa@users.noreply.github.com>
2022-11-09 17:02:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function modelLoaded() {
+console.log('Model Loaded!');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+var previous_result = '';
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        i
+    }
+}
